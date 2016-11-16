03b9ec69f1b3b301f319a5f63d7cff4e52e714cd
Jakub Krajniak <jkrajniak@gmail.com>
2016-11-16 10:12:48 +0100

    update args

--- src/tools.py
+++ src/tools.py
@@ -77,26 +77,45 @@ def save_forcefield(h5, gt):
 def _args():
     parser = MyArgParser(description='Runs classical MD simulation',
                          fromfile_prefix_chars='@')
-    parser.add_argument('--conf', required=True, help='Input .gro coordinate file')
-    parser.add_argument('--top', '--topology', required=True, help='Topology file',
+    general_options = parser.add_argument_group('General options')
+    general_options.add_argument('--conf', required=True, help='Input .gro coordinate file')
+    general_options.add_argument('--top', '--topology', required=True, help='Topology file',
                         dest='top')
-    parser.add_argument('--node_grid')
-    parser.add_argument('--skin', type=float, default=0.16,
+    general_options.add_argument('--node_grid')
+    general_options.add_argument('--skin', type=float, default=0.16,
                         help='Skin value for Verlet list')
-    parser.add_argument('--kb', type=float, default=0.0083144621, help='Boltzmann constant, default kJ/mol')
-    parser.add_argument('--mass_factor', type=float, default=1.6605402, help='Mass units, default a.u.')
-    parser.add_argument('--run', type=int, default=10000,
-                        help='Number of simulation steps')
-    parser.add_argument('--int_step', default=1000, type=int, help='Steps in integrator')
-    parser.add_argument('--rng_seed', type=int, help='Seed for RNG', required=False,
-                        default=random.randint(1000, 10000))
-    parser.add_argument('--output_prefix',
+    general_options.add_argument('--output_prefix',
                         default='sim', type=str,
                         help='Prefix for output files')
-    parser.add_argument('--output_file',
+    general_options.add_argument('--output_file',
                         default='trjout.h5', type=str,
                         help='Name of output trajectory file')
-    args_simulation_options = parser.add_argument_group('Simulation options')
+    general_options.add_argument('--trj_collect', default=1000, type=int,
+                        help='Collect trajectory every (step)')
+    general_options.add_argument('--energy_collect', default=1000, type=int,
+                        help='Collect energy every (step)')
+    general_options.add_argument('--topol_collect', default=1000, type=int,
+                        help='Collect topology every (step)')
+    general_options.add_argument('--reactions', default=None,
+                        help='Configuration file with chemical reactions')
+    general_options.add_argument('--debug', default=None, help='Turn on logging mechanism')
+    general_options.add_argument('--start_ar', default=0, type=int, help='When to start chemical reactions')
+    general_options.add_argument('--stop_ar', default=-1, type=int, help='When to stop chemical reactions')
+    general_options.add_argument('--maximum_conversion',
+                        default=None,
+                        help=('The comma separated list of conditions on which '
+                              'the simulation will stop. (format: atom type symbol:max number:total number)'))
+    general_options.add_argument('--eq_steps', default=0, help=('Run simulation after conversion reached for n-steps'), type=int)
+    general_options.add_argument('--table_groups', default=None,
+                        help='The list of atom type names that should be simulated with tabulated potential.')
+    general_options.add_argument('--max_force', default=-1, type=float,
+                        help='Maximum force in the system.')
+    general_options.add_argument('--rate_arrhenius', default=False, help='Change rate based on the Arrhenius equation.',
+                        type=ast.literal_eval)
+    general_options.add_argument('--exclusion_list', default=None, help='Read exclusion list from external file')
+    general_options.add_argument('--benchmark_data', default=None, help='Store time measurement in the file')
+
+    args_simulation_options = parser.add_argument_group('Simulation parameters')
     args_simulation_options.add_argument('--kb', type=float, default=0.0083144621, help='Boltzmann constant, default kJ/mol')
     args_simulation_options.add_argument('--mass_factor', type=float, default=1.6605402, help='Mass units, default a.u.')
     args_simulation_options.add_argument('--run', type=int, default=10000,
@@ -134,17 +153,7 @@ def _args():
                         help='Kappa paramter for coulomb interactions')
     args_simulation_options.add_argument('--coulomb_cutoff', default=0.9, type=float,
                         help='Coulomb cut-off')
-    parser.add_argument('--trj_collect', default=1000, type=int,
-                        help='Collect trajectory every (step)')
-    parser.add_argument('--energy_collect', default=1000, type=int,
-                        help='Collect energy every (step)')
-    parser.add_argument('--topol_collect', default=1000, type=int,
-                        help='Collect topology every (step)')
-    parser.add_argument('--reactions', default=None,
-                        help='Configuration file with chemical reactions')
-    parser.add_argument('--debug', default=None, help='Turn on logging mechanism')
-    parser.add_argument('--start_ar', default=0, type=int, help='When to start chemical reactions')
-    parser.add_argument('--stop_ar', default=-1, type=int, help='When to stop chemical reactions')
+
     args_storing_options = parser.add_argument_group('H5MD storage')
     args_storing_options.add_argument('--store_species', default=True, type=ast.literal_eval,
                         help='Store particle types')
@@ -160,25 +169,17 @@ def _args():
                         help='Store velocity')
     args_storing_options.add_argument('--store_charge', default=False, type=ast.literal_eval,
                         help='Store charge')
+    args_storing_options.add_argument('--store_mass', default=True, type=ast.literal_eval, help='Store mass')
+    args_storing_options.add_argument('--store_res_id', default=True, type=ast.literal_eval, help='Store res_id')
     args_storing_options.add_argument('--store_pressure', default=False, type=ast.literal_eval,
                         help='Compute and store pressure')
     args_storing_options.add_argument('--store_single_precision', default=True, type=ast.literal_eval,
                         help='Write data in single precision format')
 
-    parser.add_argument('--maximum_conversion',
-                        default=None,
-                        help=('The comma separated list of conditions on which '
-                              'the simulation will stop. (format: atom type symbol:max number:total number)'))
-    parser.add_argument('--eq_steps', default=0, help=('Run simulation after conversion reached for n-steps'), type=int)
-    parser.add_argument('--table_groups', default=None,
-                        help='The list of atom type names that should be simulated with tabulated potential.')
-    parser.add_argument('--max_force', default=-1, type=float,
-                        help='Maximum force in the system.')
-    parser.add_argument('--rate_arrhenius', default=False, help='Change rate based on the Arrhenius equation.',
-                        type=ast.literal_eval)
-    parser.add_argument('--exclusion_list', default=None, help='Read exclusion list from external file')
-    parser.add_argument('--count_tuples', default=False, type=ast.literal_eval, help='Count tuples')
-    parser.add_argument('--benchmark_data', default=None, help='Store time measurement in the file')
+    args_counters = parser.add_argument_group('Counters')
+    args_counters.add_argument('--count_types', default=False, type=ast.literal_eval, help='Count particles of type')
+    args_counters.add_argument('--count_tuples', default=False, type=ast.literal_eval, help='Count tuples')
+    args_counters.add_argument('--count_types_state', default=None, help='List of particle types, state; eq. A:3,B:4')
 
     args_hybrid_bonds = parser.add_argument_group('Hybrid bonds')
     args_hybrid_bonds.add_argument('--t_hybrid_bond', default=0, type=int)
